fdd2bcc3d13fa971e8a57aab3fe90c3e6acf8991
hoaborovipqua <hoa.tmh2003@gmail.com>
2024-09-12 09:29:43 +0700

    Search feature

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,17 +1,28 @@
+const Product = require("../../models/product.model");
 const productModel = require("../../models/product.model");
 
 module.exports.index = async (req, res) => {
-
+    // Search default
     const find = {
         deleted: false
     }
 
+    // Fillter
     if (req.query.status) {
         find.status = req.query.status
     }
+    // End fillter
+
+    // Search
+
+    if(req.query.keyword){
+        const regex = new RegExp(req.query.keyword, "i");
+        find.title = regex;
+    }
+
+    // End Search  
 
     const products = await productModel.find(find);
-
 
     console.log(products);
 
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,12 +1,13 @@
 const boxFilter = document.querySelector("[box-filter]");
-if(boxFilter) {
+if (boxFilter) {
   let url = new URL(location.href); // Nhân bản url
 
+  // Filter
   // Bắt sự kiện onChange
   boxFilter.addEventListener("change", () => {
     const value = boxFilter.value;
 
-    if(value) {
+    if (value) {
       url.searchParams.set("status", value);
     } else {
       url.searchParams.delete("status");
@@ -14,12 +15,41 @@ if(boxFilter) {
 
     location.href = url.href;
   })
+  // End filter
 
   // Hiển thị lựa chọn mặc định
   const statusCurrent = url.searchParams.get("status");
-  if(statusCurrent) {
+  if (statusCurrent) {
     boxFilter.value = statusCurrent;
   }
+  // End hiển thị mặc định
+
+  // Search 
+  const formSearch = document.querySelector("[form-search]");
+  if (formSearch) {
+    let url = new URL(location.href); // Nhân bản url
+
+    formSearch.addEventListener("submit", (event) => {
+      event.preventDefault(); // Ngăn chặn hành vi mặc định: submit form
+      const value = formSearch.keyword.value;
+
+      if (value) {
+        url.searchParams.set("keyword", value); 
+      } else {
+        url.searchParams.delete("keyword");
+      }
+
+      location.href = url.href;
+    });
+
+    // Hiển thị từ khóa mặc định
+    const valueCurrent = url.searchParams.get("keyword");
+    if (valueCurrent) {
+      formSearch.keyword.value = valueCurrent;
+    }
+  }
+  // End search 
+
 }
 
 

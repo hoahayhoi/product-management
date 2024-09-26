b15a09d94a1343e4a35fcb8201c6f207813a9f4b
hoahayhoi <hoa.tmh2003@gmail.com>
2024-09-26 15:47:17 +0700

    Product category: create

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,5 +1,6 @@
 const dashboardRoute = require("./dashboard.route");
 const productRoute = require("./product.route");
+const productCategoryRoute = require("./product-category.route");
 
 const systemConfig = require("../../config/system");
 
@@ -10,4 +11,6 @@ module.exports = (app) => {
   app.use(`${PATH_ADMIN}/dashboard`, dashboardRoute);
 
   app.use(`${PATH_ADMIN}/products`, productRoute);
+
+  app.use(`${PATH_ADMIN}/products-category`, productCategoryRoute);
 }

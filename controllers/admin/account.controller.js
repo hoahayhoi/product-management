9bee24f64e4fad7083056e8e847a4cf50ce16ca5
hoahayhoi <hoa.tmh2003@gmail.com>
2024-10-02 18:27:49 +0700

    Admin: Create Acount

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -1,6 +1,39 @@
+const AccountModel = require("../../models/account.model");
+const RoleModel = require("../../models/role.model");
+const md5 = require("md5");
+
+const generateHelper = require("../../helpers/generate.helper");
+const systemConfig = require("../../config/system");
 
 module.exports.index = async (req, res) => {
-    res.render("admin/pages/accounts/index", {
-        pageTitle: "Tài khoản quản trị"
+    const accounts = await AccountModel.find({
+        deleted: false
     });
+
+    res.render("admin/pages/accounts/index", {
+        pageTitle: "Tài khoản quản trị",
+        accounts
+    });
+}
+
+module.exports.create = async (req, res) => {
+    const roles = await RoleModel.find({
+        deleted: false
+    }); 
+    res.render("admin/pages/accounts/create", {
+        pageTitle: "Tài khoản quản trị",
+        roles
+    });
+}
+
+
+module.exports.createPost = async (req, res) => {
+    req.body.password = md5(req.body.password);
+    req.body.token = generateHelper.generateRandomString(30);
+
+    const account = new AccountModel(req.body);
+
+    await account.save();
+
+    res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
 }

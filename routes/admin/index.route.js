d28ba1487ae701311a047d6362f2d0d7eacfde82
hoahayhoi <hoa.tmh2003@gmail.com>
2024-10-03 10:07:16 +0700

    feat: Login

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,6 +3,7 @@ const productRoute = require("./product.route");
 const productCategoryRoute = require("./product-category.route");
 const roleRoute = require("./role.route");
 const accountRoute = require("./account.route");
+const authRoute = require("./auth.route");
 
 const systemConfig = require("../../config/system");
 
@@ -19,4 +20,6 @@ module.exports = (app) => {
   app.use(`${PATH_ADMIN}/roles`, roleRoute);
 
   app.use(`${PATH_ADMIN}/accounts`, accountRoute);
+
+  app.use(`${PATH_ADMIN}/auth`, authRoute);
 }

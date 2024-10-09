60a558ca3bc6dd39c395d19c911b6ec05956b5ed
hoahayhoi <hoa.tmh2003@gmail.com>
2024-10-09 23:20:30 +0700

    Client: get child products of parent category

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -5,10 +5,10 @@ module.exports.index = async (req, res) => {
   const products = await Product.find({
     status: 'active',
     deleted: false
-  }).sort({positon: 'desc'});
+  }).sort({ positon: 'desc' });
 
   for (const item of products) {
-    item.priceNew = item.price*(100 - item.discountPercentage)/100;
+    item.priceNew = item.price * (100 - item.discountPercentage) / 100;
     item.priceNew = (item.priceNew).toFixed(0);
   }
 
@@ -28,7 +28,7 @@ module.exports.detail = async (req, res) => {
     deleted: false
   });
 
-  product.priceNew = product.price*(100 - product.discountPercentage)/100;
+  product.priceNew = product.price * (100 - product.discountPercentage) / 100;
   product.priceNew = (product.priceNew).toFixed(0);
   res.render("client/pages/products/detail", {
     pageTitle: product.title,
@@ -38,21 +38,40 @@ module.exports.detail = async (req, res) => {
 
 module.exports.category = async (req, res) => {
   const slugCategory = req.params.slugCategory;
-  
+
   const category = await ProductCategory.findOne({
     slug: slugCategory,
     deleted: false,
     status: "active"
   })
+
+  const allCategoryChildren = [];
+  const getCategoryChildren = async (parentId) => {
+    const childs = await ProductCategory.find({
+      parent_id: parentId,
+      status: "active",
+      deleted: false
+    });
+
+    for (const child of childs) {
+      allCategoryChildren.push(child.id);
+      await getCategoryChildren(child.id);
+    }
+  };
+
+  await getCategoryChildren(category.id);
+
   const products = await Product.find({
-    category_id: category.id,
+    category_id: { $in: [category.id, ...allCategoryChildren] },
     status: "active",
     deleted: false
   }).sort({ position: "desc" });
+
   for (const product of products) {
-    product.priceNew = product.price*(100 - product.discountPercentage)/100;
+    product.priceNew = product.price * (100 - product.discountPercentage) / 100;
     product.priceNew = (product.priceNew).toFixed(0);
   }
+  
   res.render("client/pages/products/index", {
     pageTitle: category.title,
     products: products

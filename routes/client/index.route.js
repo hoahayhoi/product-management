bb98f6b3798d92b22c02d7fa65169f6f2aa7a785
hoahayhoi <hoa.tmh2003@gmail.com>
2024-10-10 10:37:12 +0700

    Client: add product to cart

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,12 +1,20 @@
 const homeRoute = require("./home.route");
 const productRoute = require("./product.route");
+const cartRoute = require("./cart.route");
+
 
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
+const cartMiddleware = require("../../middlewares/client/cart.middleware");
+
 
 module.exports = (app) => {
   app.use(categoryMiddleware.category); // áp dụng middleware cho các route còn lại 
 
+  app.use(cartMiddleware.cart);
+
   app.use("/", homeRoute);
   
   app.use("/products", productRoute);
+
+  app.use("/cart", cartRoute);
 }

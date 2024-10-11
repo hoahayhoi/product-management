8e3c71eeb888c6b17fe0df2cb128d05a519c64cc
hoahayhoi <hoa.tmh2003@gmail.com>
2024-10-11 11:23:22 +0700

    Client: order success page

--- routes/client/order.route.js
+++ routes/client/order.route.js
@@ -4,5 +4,6 @@ const controller = require("../../controllers/client/order.controller");
 
 router.get("/", controller.index);
 router.post("/", controller.orderPost);
+router.get("/success/:id", controller.success);
 
 module.exports = router;
